a3ac53965d087184f224c38ffdb2aaa7b838df27
suganuma3510 <suganuma3510@gmail.com>
2022-03-18 22:01:14 +0900

    fix：ALB修正

--- ECS-on-Fargate/module/elb/main.tf
+++ ECS-on-Fargate/module/elb/main.tf
@@ -14,11 +14,12 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "alb-tg" {
-  name        = "${var.name}-alb-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
+  name                 = "${var.name}-alb-tg"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "ip"
+  deregistration_delay = 300
 
   health_check {
     enabled             = true
