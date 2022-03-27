4d50f3e367898312c9f62021545b550bccc59d30
suganuma3510 <suganuma3510@gmail.com>
2022-03-27 23:06:44 +0900

    fix：RDSのパスワードをSecrets Managerで管理するように修正

--- RDS/module/rds/main.tf
+++ RDS/module/rds/main.tf
@@ -48,8 +48,38 @@ resource "aws_db_instance" "db" {
   instance_class         = var.db_instance
   identifier             = var.db_name
   username               = var.db_username
-  password               = var.db_password
+  password               = data.aws_secretsmanager_secret_version.db_password.secret_string
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
+}
+
+#--------------------------------------------------------------
+# Secrets Manager
+#--------------------------------------------------------------
+
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "${var.app_name}-db-password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = random_password.db_password.result
+}
+
+data "aws_secretsmanager_secret" "db_password" {
+  name = "${var.app_name}-db-password"
+  depends_on = [
+    aws_secretsmanager_secret.db_password
+  ]
+}
+
+data "aws_secretsmanager_secret_version" "db_password" {
+  secret_id = data.aws_secretsmanager_secret.db_password.id
 }
