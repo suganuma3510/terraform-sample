97daa7f0625c85e367ac9b9f28bd73650b900028
Mikoto Suganuma <mikoto_suganuma@yaz.co.jp>
2023-03-11 16:39:03 +0900

    add：RDS-Auroraのセキュリティグループを変更、その他リファクタリング

--- RDS-Aurora/module/rds/main.tf
+++ RDS-Aurora/module/rds/main.tf
@@ -24,7 +24,7 @@ resource "aws_rds_cluster" "default" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster_instance
 resource "aws_rds_cluster_instance" "default" {
-  count = 2
+  count = var.db_instance_count
 
   identifier         = "${var.db_name}-aurora-cluster-instance-${count.index}"
   cluster_identifier = aws_rds_cluster.default.id
@@ -54,6 +54,7 @@ resource "aws_rds_cluster_parameter_group" "default" {
 # Subnet group
 #--------------------------------------------------------------
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_subnet_group
 resource "aws_db_subnet_group" "rds" {
   name        = var.db_name
   description = "rds subnet group for ${var.db_name}"
@@ -64,26 +65,44 @@ resource "aws_db_subnet_group" "rds" {
 # Security group
 #--------------------------------------------------------------
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 resource "aws_security_group" "rds" {
   name        = "${var.app_name}-rds-sg"
   description = "RDS service security group for ${var.app_name}"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "${var.app_name}-rds-sg"
   }
+}
+
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "rds_ingress_mysql" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = var.pri_subnet_cidr_blocks
+  security_group_id = aws_security_group.rds.id
+}
+
+resource "aws_security_group_rule" "rds_ingress_app" {
+  count = length(var.source_security_group_ids)
+
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = element(var.source_security_group_ids, count.index)
+  security_group_id        = aws_security_group.rds.id
+}
+
+resource "aws_security_group_rule" "rds_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds.id
 }
