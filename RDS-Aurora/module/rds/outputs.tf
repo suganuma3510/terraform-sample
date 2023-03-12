a1568252f7e3d1fbe528f3e7b262cadddbda405a
suganuma3510 <suganuma3510@gmail.com>
2023-03-12 23:24:29 +0900

    add：RDS-Auroraのoutputを追加、その他リファクタリング

--- RDS-Aurora/module/rds/outputs.tf
+++ RDS-Aurora/module/rds/outputs.tf
@@ -1,3 +1,27 @@
-output "db_address" {
+output "db_security_group_id" {
+  value = aws_security_group.rds.id
+}
+
+output "db_endpoint" {
   value = aws_rds_cluster.default.endpoint
+}
+
+output "db_reader_endpoint" {
+  value = aws_rds_cluster.default.reader_endpoint
+}
+
+output "db_engine" {
+  value = aws_rds_cluster.default.engine
+}
+
+output "db_name" {
+  value = aws_rds_cluster.default.database_name
+}
+
+output "rds_cluster_id" {
+  value = aws_rds_cluster.default.id
+}
+
+output "rds_cluster_instance_ids" {
+  value = aws_rds_cluster_instance.default.*.id
 }
