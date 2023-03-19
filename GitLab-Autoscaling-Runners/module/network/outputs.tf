08341acddf455467bb4b57371add74d677c191e7
Mikoto Suganuma <mikoto_suganuma@yaz.co.jp>
2023-03-19 16:31:36 +0900

    remove：GitLab-Autoscaling-Runnersのファイルを削除

--- GitLab-Autoscaling-Runners/module/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" { value = aws_vpc.vpc.id }
-
-output "vpc_cidr" { value = aws_vpc.vpc.cidr_block }
-
-output "pub_subnet_ids" { value = aws_subnet.pub-sub.*.id }
-
-output "ec2_security_group" { value = aws_security_group.ec2-sg }
