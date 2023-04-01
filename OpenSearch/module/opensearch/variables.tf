fb3541bbcf3ce15b35a4c807b162b0668317be2f
suganuma3510 <suganuma3510@gmail.com>
2023-04-02 00:58:26 +0900

    add：OpenSearch VPCのTerraformサンプルを追加

--- OpenSearch/module/opensearch/variables.tf
+++ OpenSearch/module/opensearch/variables.tf
@@ -4,6 +4,19 @@ variable "username" {}
 
 variable "password" {}
 
+variable "vpc_id" {}
+
+variable "pri_subnet_ids" {}
+
+variable "pri_subnet_cidr_blocks" {
+  type = list(string)
+}
+
+variable "source_security_group_ids" {
+  type    = list(string)
+  default = []
+}
+
 variable "elasticsearch_version" {
   default     = "OpenSearch_1.3"
   description = "check available engines with 'aws opensearch list-versions'"
