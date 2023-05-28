16ed3c5978618f15bd4f2f70984d8e6415d456ba
suganuma3510 <suganuma3510@gmail.com>
2023-05-28 21:47:11 +0900

    add：テスト用LBを追加

--- ALB/module/elb/outputs.tf
+++ ALB/module/elb/outputs.tf
@@ -1,7 +1,15 @@
+output "lb_tg_arn" {
+  value = aws_lb_target_group.default.arn
+}
+
 output "http_listener_arn" {
-  value = aws_alb_listener.http.arn
+  value = aws_lb_listener.http.arn
 }
 
 # output "https_listener_arn" {
-#   value = aws_alb_listener.https.arn
+#   value = aws_lb_listener.https.arn
 # }
+
+output "lb_security_group_id" {
+  value = aws_security_group.alb.id
+}
